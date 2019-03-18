a572eaf5837ecc266c36a442ca07880cf134fcc7
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-03-18 16:10:55 -0400

    Header Title

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return(
+        <div className="header-title">
+            <h1>Lambda School</h1>
+            <a href="https://lambdaschool.com/">LambdaSchool - 26 jan</a>
+        </div>
+    )
+}
+
+export default HeaderTitle;
